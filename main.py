72578fbb181c3b6688d9d2e324fdd1378be7a7a5
Milda Auglyte <auglyte.m@gmail.com>
2023-04-20 14:49:48 +0300

    pagrindinis

--- main.py
+++ main.py
@@ -20,21 +20,3 @@
 # pajamų įrašo sukūrimas ir įtraukimui į žurnalą
 # išlaidų įrašo sukūrimas ir įtraukimui į žurnalą
 # Programos meniu ir funkcionalumą galite įgyvendinti tiek per klasę, tiek pagrindinėje programoje.
-
-
-def add_product(product_name, count, list_name):  # Karolis Jasadavičius
-# add products, 
-# if product is solid unit == kg
-# if product is solid unit == litres (l)
-
-def remove_product(product_name, count, list_name):  # Karolis Jasadavičius
-# product count reduce
-
-def remove_if_zero():  # Karolis Venckus
-# this probably should run after remove_product() eg. if product == 'cheese': 0 ....
-
-def view_product_list():  # Karolis Venckus
-# print listed products and counts with respective units
-
-def calculate_fridge_mass():  # Milda Auglytė
-# sum products count (1kg == 1l, 3kg == 3l ...)
