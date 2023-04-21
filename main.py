1b25e144776ce51a3d7c2e846572d72048bd35a7
RenaldasZ <spikeris2@gmail.com>
2023-04-21 08:11:20 +0300

    update, pridetos spalvos

--- main.py
+++ main.py
@@ -1,42 +1,27 @@
-# UZDUOTIS
-
-# Irasas (abstraktus)
-
-# savybė suma
-# savybė komentaras
-# Islaidos(Irasas)
-
-# savybė gavejas
-# Pajamos(Irasas)
-
-# savybė siuntejas
-# Biudzetas
-
-# savybe zurnalas: sąrašas įrašų
-# Biudzetas turi turėti metodus:
-
-# ataskaitai
-# balansas
-# pajamų įrašo sukūrimas ir įtraukimui į žurnalą
-# išlaidų įrašo sukūrimas ir įtraukimui į žurnalą
-# Programos meniu ir funkcionalumą galite įgyvendinti tiek per klasę, tiek pagrindinėje programoje.
-
-
-
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
 class Irasas:
-    def __init__(self, suma, komentaras):
+    def __init__(self, suma: float, komentaras: str) -> str:
+        """
+        Inicializuoja 'Irasas' klasės objektą su nurodytais kintamaisiais.
+
+        :param suma: 'Irasas' objekto suma.
+        :type suma: float
+        :param komentaras: 'Irasas' objekto komentaras.
+        :type komentaras: str
+        """
         self.suma = suma
         self.komentaras = komentaras
 
 
 class Islaidos(Irasas):
-    def __init__(self, suma, komentaras, gavejas):
+    def __init__(self, suma: float, komentaras: str, gavejas: str):
         super().__init__(suma, komentaras)
         self.gavejas = gavejas
 
 
 class Pajamos(Irasas):
-    def __init__(self, suma, komentaras, siuntejas):
+    def __init__(self, suma: float, komentaras: str, siuntejas: str):
         super().__init__(suma, komentaras)
         self.siuntejas = siuntejas
 
@@ -44,49 +29,87 @@ class Pajamos(Irasas):
 class Biudzetas:
     zurnalas = []
 
-    def ataskaita(self):
+    def ataskaita(self) -> str:
+        """
+        Atspausdina biudžeto žurnalą.
+        """
+        print(biudzetas.get_balansas_string())
         for irasas in self.zurnalas:
-            print(f'Ataskaita: Suma: {irasas.suma}, komentaras: {irasas.komentaras}')
+            if isinstance(irasas, Pajamos):
+                print("")
+                print(f'\033[1;32;40m-=Pajamos\033[0m: Suma: {irasas.suma} €, komentaras: {irasas.komentaras}, siuntėjas: {irasas.siuntejas}')
+            elif isinstance(irasas, Islaidos):
+                print(f'\033[1;31;40m-=Išlaidos\033[0m: Suma: {irasas.suma} €, komentaras: {irasas.komentaras}, gavėjas: {irasas.gavejas}')
 
     def balansas(self):
+        """
+        Atspausdina balansą.
+        """
         pajamos = sum([irasas.suma for irasas in self.zurnalas if isinstance(irasas, Pajamos)])
         islaidos = sum([irasas.suma for irasas in self.zurnalas if isinstance(irasas, Islaidos)])
         return pajamos - islaidos
     
-    def itraukti_irasa(self, irasas):
+    def get_balansas_string(self) -> str:
+        """
+        Grąžina eilutę, nurodančią dabartinį biudžeto balansą
+        Returns a string indicating the current balance of the budget.
+        """
+        balansas = self.balansas()
+        if balansas == 0:
+            return "\033[1;31;40mJūsų biudžeto sąrašas yra tuščias..\033[0m"
+        else:
+            return f"\033[1;32;40m-=Bendras balansas\033[0m: {balansas} €"
+    
+    def ivesti_pajamas(self, irasas):
         self.zurnalas.append(irasas)
+
 
 biudzetas = Biudzetas()
 
-def ivesti_pajamas(biudzetas):
-    inesta_suma = int(input('Inesta suma: '))
-    operacijos_komentaras = input('Operacijos komentaras: ')
-    siuntejas = input('Siuntejas: ')
 
-def ivesti_islaidas(biudzetas):
-    isimta_suma = int(input('Isimta suma: '))
-    operacijos_komentaras = input('Operacijos komentaras: ')
-    siuntejas = input('Siuntejas: ')
+def ivesti_pajamas(biudzetas: Biudzetas) -> None:
+    print("")
+    print("Įveskite \033[1;32;40m gautų \033[0m pajamų sumą pvz. 1250.04 ir spauskite 'Enter':")
+    suma = float(input("-->>> "))
+    print("Įveskite pajamų siuntėją ir spauskite 'Enter':")
+    siuntejas = input("-->>> ")
+    print("Įveskite komentarą ir spauskite 'Enter':")
+    komentaras = input("-->>> ")
+    pajamos = Pajamos(suma, komentaras, siuntejas)
+    biudzetas.ivesti_pajamas(pajamos)
+    print("")
+    print(f"Pajamos {suma} € buvo pridėtos prie biudžeto.")
+    print(biudzetas.get_balansas_string())
 
-def __str__(self):
-    return f'Jus ivedete: {biudzetas.ivesti_pajamas}, jusu balansas {Irasas.balansas}'
-
+def ivesti_islaidas(biudzetas: Biudzetas) -> None:
+    suma = float(input("Įveskite išlaidų sumą: "))
+    gavejas = input("Įveskite išlaidų gavėją: ")
+    komentaras = input("Įveskite komentarą: ")
+    islaidos = Islaidos(suma, komentaras, gavejas)
+    biudzetas.ivesti_pajamas(islaidos)
+    print(f"Įšlaidos {suma} € buvo išimta iš biudžeto.")
+    print(biudzetas.get_balansas_string())
 
 while True:
-    print('------------Biudzetas------------')
-    print("\nPasirinkite veiksma:")
-    print("1. Sukurti pajamų irasa")
-    print("2. Sukurti islaidu irasa")
-    print("3. Ataskaita")
-    print(" ")
-    print("0. Baigti programa")
-    print(" ")
-    print(" ")
-    meniu = int(input('Jusu pasirinkimas'))  
+    print("\n\t\033[1;36;40m-----=== Biudžeto skaičiavimo programa ===-----\033[0m\n")
+    print("\t\t\033[1;32;40m--= Pasirinkite veiksmą =--\033[0m\n")
+    print("\t\033[1;32;40m1\033[0m. Sukurti pajamų įrašą")
+    print("\t\033[1;32;40m2\033[0m. Sukurti išlaidų įrašą")
+    print("\t\033[1;32;40m3\033[0m. Ataskaita\n")
+ 
+    print("\t\033[1;31;40m0\033[0m. Uždaryti programą\n")
+    meniu = int(input('Įveskite pasirinkimą \033[1;32;40m1\033[0m, \033[1;32;40m2\033[0m, \033[1;32;40m3\033[0m arba \033[1;31;40m0\033[0m ir spauskite "Enter": '))
 
-
-
-
-    
+    if meniu == 1:
+        ivesti_pajamas(biudzetas)
+    elif meniu == 2:
+        ivesti_islaidas(biudzetas)
+    elif meniu == 3:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        biudzetas.ataskaita() 
+    elif meniu == 0:
+        break
+    else:
+        print("\n\033[1;31;40mNeteisingas pasirinkimas, bandykite dar kartą!\033[0m\n")
 
 
